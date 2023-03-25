6fb0ef0da01855249042d92f5a88689c5d2243d8
roberto <61357564+robahtou@users.noreply.github.com>
2023-03-25 12:13:11 -0500

    reset main

--- src/components/Icons/ArrowRightIcon/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function ArrowRight({ className}: {className: string}) {
-
-  return (
-    <svg className={className} width="22" height="16" viewBox="0 0 22 16" fill="none">
-      <path d="M1.66602 8.00016H20.3327M20.3327 8.00016L13.3327 1.3335M20.3327 8.00016L13.3327 14.6668" strokeWidth="2.66667" strokeLinecap="round" strokeLinejoin="round"/>
-    </svg>
-
-  )
-}
