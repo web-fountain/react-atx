6ad0e7be57151813884a804cc0af7eb66d22df98
roberto <61357564+robahtou@users.noreply.github.com>
2023-04-08 09:21:50 -0500

    update component Icons

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,30 @@
+import ArrowUpRight from './ArrowUpRight';
+import DiscordWhite from './DiscordWhite';
+import Lightbulb from './Lightbulb';
+import Moon from './Moon';
+import ReactLogo from './ReactLogo';
+import Sun from './Sun';
+
+
+type Props = {
+  name: string;
+  className?: string;
+  inlineFill?: string;
+  inlineStroke?: string;
+};
+
+function Icons({ name, ...props }: Props): JSX.Element {
+  const icons: Record<Props['name'], any> = {
+    'arrow-up-right': <ArrowUpRight {...props} />,
+    'lightbulb': <Lightbulb {...props} />,
+    'discord-white': <DiscordWhite {...props} />,
+    'moon': <Moon {...props} />,
+    'react-logo': <ReactLogo {...props} />,
+    'sun': <Sun {...props} />
+  };
+
+  return icons[name];
+}
+
+
+export default Icons;
