4e32ff3fa0d25faa7b9d78fad2c09c4100e594fa
roberto <61357564+robahtou@users.noreply.github.com>
2023-04-08 09:18:07 -0500

    favicon: replace vercel

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import '@Styles/fonts.css';
+import '@Styles/resets.css';
+import '@Styles/base.css';
+import '@Styles/layout.css';
+import '@Styles/themes.css';
+
+import { ReactNode } from 'react';
+import { Footer, Header } from '@Components';
+
+
+function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html data-theme="dark" lang="en-US">
+      <head>
+        {/* #Favicon.1 */}
+        <link rel="icon" sizes="any" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml"  href="/favicon/icon.svg" />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="favicon/manifest.webmanifest" />
+
+        <style>@layer fonts, resets, base, layout, page, theme;</style>
+      </head>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
+
+
+export default RootLayout;
