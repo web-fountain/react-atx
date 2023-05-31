912ee2282e7cb35cbd0ffa73ff999302b4ced5a0
Nikita Kruzhilin <myswod@gmail.com>
2023-05-31 09:16:55 -0500

    Refactored code following Core rules

--- src/app/news/Filters/index.tsx
+++ src/app/news/Filters/index.tsx
@@ -4,29 +4,28 @@ import styles from './styles.module.css';
 
 
 const filters = [
-    'React',
-    'CSS',
-    'JavaScript',
+  'React',
+  'CSS',
+  'JavaScript'
 ];
 
 function Filters(props: { setFilter: Function, filter: String }) {   
-    return (
-        <ul className={styles['filters-container']}>
-            {filters.map(element =>
-                <li key={element}>
-                    <button
-                        className={element.toLowerCase() === props.filter
-                            ? `${styles.filter} ${styles.active}` 
-                            : `${styles.filter}`}
-                        onClick={() => props.setFilter(element.toLowerCase())}
-                    >
-                        {element}
-                    </button>
-                </li>
-            )}
-            
-        </ul>
-    );
+  return (
+    <ul className={styles['filters-container']}>
+      {filters.map(element =>
+        <li key={element}>
+          <button
+            className={element.toLowerCase() === props.filter
+              ? `${styles.filter} ${styles.active}` 
+              : `${styles.filter}`}
+            onClick={() => props.setFilter(element.toLowerCase())}
+          >
+            {element}
+          </button>
+        </li>
+      )}        
+    </ul>
+  );
 };
 
 export default Filters;
