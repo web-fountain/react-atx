2d530516d5cfd3212f6112975eeb0cb6a045777f
Nikita Kruzhilin <myswod@gmail.com>
2023-05-31 07:58:51 -0500

    removed the background image

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import NewsList from './NewsList';
 
+import styles from './styles.module.css';
+
 
 export const metadata = {
   title: 'React ATX | News'
@@ -8,7 +10,7 @@ export const metadata = {
 
 function News() {
   return (
-    <main className="main-layout">
+    <main className={`${styles.news} main-layout`}>
       <h2>News</h2>
       <NewsList/>
     </main>
