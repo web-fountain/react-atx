edf14fa2cadd9bcce845bceec8e869146791792c
Carlos Valderrama <cvalderrama1991@gmail.com>
2023-06-08 22:46:20 -0400

    Generalized the button and link component

--- src/app/(landingPage)/Hero/index.tsx
+++ src/app/(landingPage)/Hero/index.tsx
@@ -1,25 +1,28 @@
 import Icons from '@Icons';
 import styles from './styles.module.css';
-
+import ButtonLink from '@/components/ButtonLink';
 
 function Hero() {
   return (
     <section id='hero' className={styles.layout}>
       <h4>React ATX</h4>
-      <h3>Join Austin's <br></br>React Community</h3>
-      <p>We're a community of developers that come together to learn, share, and explore the world of frontend with ReactJS.</p>
-
-      <a className={styles.container} href="#join">
-        <div className={styles['text-box']}>
-          <span>
-            Join now
-            <Icons name="arrow-up-right" />
-          </span>
-        </div>
-      </a>
+      <h3>
+        Join Austin's <br></br>React Community
+      </h3>
+      <p>
+        We're a community of developers that come together to learn, share, and
+        explore the world of frontend with ReactJS.
+      </p>
+      <ButtonLink
+        element='anchor'
+        path='#join'
+        height={64}
+        width={260}
+        icon='arrow-up-right'
+        text='Join now'
+      />
     </section>
   );
 }
-
 
 export default Hero;
---
+++ src/components/ButtonLink/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import Link from 'next/link';
+import Icons from '@Icons';
+import styles from './styles.module.css';
+
+interface Props {
+  element: 'next-link' | 'anchor' | 'button';
+  text: string;
+  icon: string;
+  height: number;
+  width: number;
+  path?: string;
+  type?: 'submit' | 'button';
+}
+
+export default function ButtonLink({
+  element,
+  path,
+  type,
+  text,
+  icon,
+  height,
+  width
+}: Props) {
+  if (element === 'next-link') {
+    return (
+      <Link
+        href={`${path}`}
+        style={{ height: height + 'px', maxWidth: width + 'px' }}
+        className={styles.container}
+      >
+        <div className={styles['text-box']}>
+          <span>
+            {text}
+            <Icons name={`${icon}`} />
+          </span>
+        </div>
+      </Link>
+    );
+  } else if (element === 'anchor') {
+    return (
+      <a
+        href={`${path}`}
+        style={{ height: height + 'px', maxWidth: width + 'px' }}
+        className={styles.container}
+      >
+        <div className={styles['text-box']}>
+          <span>
+            {text}
+            <Icons name={`${icon}`} />
+          </span>
+        </div>
+      </a>
+    );
+  } else {
+    return (
+      <button
+        type={type}
+        style={{ height: height + 'px', maxWidth: width + 'px' }}
+        className={styles.container}
+      >
+        <div className={styles['text-box']}>
+          <span>
+            {text}
+            <Icons name={`${icon}`} />
+          </span>
+        </div>
+      </button>
+    );
+  }
+}
