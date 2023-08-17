6a83a0863ba6ac009914bd401243483d42a9ef21
Carlos Valderrama <cvalderrama1991@gmail.com>
2023-08-16 20:51:24 -0400

    fixed the padding on the hero page

--- src/app/(landingPage)/Hero/index.tsx
+++ src/app/(landingPage)/Hero/index.tsx
@@ -7,12 +7,16 @@ function Hero() {
   return (
     <section id='hero' className={styles.hero}>
       <div className={styles.intro}>
+        <h4>React ATX</h4>
+        <h3>
+          Join Austin's <br></br>React Community
+        </h3>
+        <p>
+          We're a community of developers that come together to learn, share,
+          and explore the world of frontend with ReactJS.
+        </p>
 
-      <h4>React ATX</h4>
-      <h3>Join Austin's <br></br>React Community</h3>
-      <p>We're a community of developers that come together to learn, share, and explore the world of frontend with ReactJS.</p>
-
-      <ButtonLink
+        <ButtonLink
           element='anchor'
           path='#join'
           height={64}
