0a59edc7a5b2cd8b7bd7e54b099e9ac04804c7b9
Carlos Valderrama <cvalderrama1991@gmail.com>
2024-05-31 12:54:23 -0400

    SVG for join top background replace with a background linear gradient

--- src/app/(landingPage)/Join/form.tsx
+++ src/app/(landingPage)/Join/form.tsx
@@ -72,85 +72,78 @@
 //   )
 // }
 
-"use client";
+'use client';
 
-import useForm from "@Hooks/useForm";
-import joinEmailSchema from "./schema";
-import joinEmailAction from "./actions";
+import useForm from '@Hooks/useForm';
+import joinEmailSchema from './schema';
+import joinEmailAction from './actions';
 
-import SubmitButton from "@Components/SubmitButton";
-import styles from "./styles.module.css";
+import SubmitButton from '@Components/SubmitButton';
+import styles from './styles.module.css';
 
 function JoinForm() {
-	// NOTE: if user does not use `handleSubmit`, they will need to `setErrors`
-	const { handleAction, handleSubmit, actionState, isPending, errors } =
-		useForm({
-			formSchema: joinEmailSchema,
-			formAction: joinEmailAction,
-			initialFormActionState: { success: false, errors: null, data: null },
-		});
+  // NOTE: if user does not use `handleSubmit`, they will need to `setErrors`
+  const { handleAction, handleSubmit, actionState, isPending, errors } = useForm({
+    formSchema: joinEmailSchema,
+    formAction: joinEmailAction,
+    initialFormActionState: { success: false, errors: null, data: null }
+  });
 
-	return (
-		<div className={styles["content"]}>
-			<h1>
-				Join the
-				<br />
-				Community
-			</h1>
+  return (
+    <div className={styles['content']}>
+      <h1>
+        Join the
+        <br />
+        Community
+      </h1>
 
-			{actionState.success ? (
-				<AlmostDone email={actionState.data?.email} />
-			) : (
-				<>
-					<p>Subscribe to our newsletter and get notified of upcoming events</p>
-					<form
-						id="JoinForm"
-						name="JoinForm"
-						className={styles["form"]}
-						action={handleAction} // server
-						onSubmit={handleSubmit} // client
-					>
-						<p className={styles["error-message"]}>{errors.email?.message}</p>
+      {actionState.success ? (
+        <AlmostDone email={actionState.data?.email} />
+      ) : (
+        <>
+          <p>Subscribe to our newsletter and get notified of upcoming events</p>
+          <form
+            id="JoinForm"
+            name="JoinForm"
+            className={styles['form']}
+            action={handleAction} // server
+            onSubmit={handleSubmit} // client
+          >
+            <p className={styles['error-message']}>{errors.email?.message}</p>
 
-						<div className={styles["actions"]}>
-							<input
-								id="email"
-								name="email"
-								type="text"
-								minLength={6}
-								placeholder="example@mail.com"
-								required={true}
-							/>
+            <div className={styles['actions']}>
+              <input id="email" name="email" type="text" minLength={6} placeholder="example@mail.com" required={true} />
 
-							<div className={styles["button-wrapper"]}>
-								<SubmitButton
-									disabled={isPending}
-									formId="JoinForm"
-									label="Submit"
-								/>
-							</div>
-						</div>
-					</form>
-				</>
-			)}
-		</div>
-	);
+              <div className={styles['button-wrapper']}>
+                <SubmitButton
+                  disabled={isPending}
+                  formId="JoinForm"
+                  label="Submit"
+                  style={{ backgroundColor: 'var(--bg-color-white)', color: 'var(--primary-color)' }}
+                />
+              </div>
+            </div>
+          </form>
+        </>
+      )}
+    </div>
+  );
 }
 
 function AlmostDone({ email }: { email: string }) {
-	return (
-		<div className={styles["almost-done"]}>
-			<h1>Almost Done!</h1>
-			<h2>Check your email</h2>
-			<p>
-				We sent you an email at
-				<br />
-				<span className={styles["email-sent-to"]}>{email}</span>
-				<br />
-				Click the link to finish your registration
-			</p>
-		</div>
-	);
+  return (
+    <div className={styles['almost-done']}>
+      <h1>Almost Done!</h1>
+      <h2>Check your email</h2>
+      <p>
+        We sent you an email at
+        <br />
+        <span className={styles['email-sent-to']}>{email}</span>
+        <br />
+        Click the link to finish your registration
+      </p>
+    </div>
+  );
 }
 
 export default JoinForm;
