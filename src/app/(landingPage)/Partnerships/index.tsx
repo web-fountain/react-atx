6c1a718ce9c03591901f3936044afb8fa13ab236
Carlos Valderrama <cvalderrama1991@gmail.com>
2024-07-04 22:24:00 -0400

    styling and SubmitButton correcctions

--- src/app/(landingPage)/Partnerships/index.tsx
+++ src/app/(landingPage)/Partnerships/index.tsx
@@ -1,158 +1,158 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { useFormStatus } from "react-dom";
+import { useState } from 'react';
+import { useFormStatus } from 'react-dom';
 
-import TextInput from "@Components/TextInput";
-import SponsorSelection from "@Components/SponsorSelection";
-import Button from "@Components/SubmitButton";
+import TextInput from '@Components/TextInput';
+import SponsorSelection from '@Components/SponsorSelection';
+import SubmitButton from '@Components/SubmitButton';
 
-import styles from "./styles.module.css";
+import styles from './styles.module.css';
 
 interface FormDataTypes {
-	name: string;
-	jobTitle: string;
-	email: string;
-	company: string;
-	venue: boolean;
-	food: boolean;
-	drinks: boolean;
-	speakers: boolean;
-	workshops: boolean;
-	other: boolean;
+  name: string;
+  jobTitle: string;
+  email: string;
+  company: string;
+  venue: boolean;
+  food: boolean;
+  drinks: boolean;
+  speakers: boolean;
+  workshops: boolean;
+  other: boolean;
 }
 
 function Partnerships() {
-	const { pending } = useFormStatus();
-	const [formData, setFormData] = useState<FormDataTypes>({
-		name: "",
-		jobTitle: "",
-		email: "",
-		company: "",
-		venue: false,
-		food: false,
-		drinks: false,
-		speakers: false,
-		workshops: false,
-		other: false,
-	});
+  const { pending } = useFormStatus();
+  const [formData, setFormData] = useState<FormDataTypes>({
+    name: '',
+    jobTitle: '',
+    email: '',
+    company: '',
+    venue: false,
+    food: false,
+    drinks: false,
+    speakers: false,
+    workshops: false,
+    other: false
+  });
 
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value, type, checked } = e.currentTarget;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.currentTarget;
 
-		setFormData((prevData) => ({
-			...prevData,
-			[name]: type === "checkbox" ? !prevData[name] : value,
-		}));
-	};
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'checkbox' ? !checked : value
+    }));
+  };
 
-	const handleSubmit = (e: React.FormEvent) => {
-		e.preventDefault();
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
 
-		console.log("formdata", formData);
-	};
+    console.log('formdata', formData);
+  };
 
-	return (
-		<section id="partnership" className={styles["layout"]}>
-			<div>
-				<h2>Partnership</h2>
-				<h3>Interested in sponsoring?</h3>
+  return (
+    <section id="partnership" className={styles['layout']}>
+      <div>
+        <h2>Partnership</h2>
+        <h3>Interested in sponsoring?</h3>
 
-				<form className={styles["form"]} onSubmit={handleSubmit}>
-					<ul className={styles["text-input-list"]}>
-						<TextInput
-							id="name"
-							name="name"
-							label="Name*"
-							placeholder="name..."
-							value={formData.name}
-							onChange={handleChange}
-						/>
-						<TextInput
-							id="email"
-							name="email"
-							label="Email*"
-							placeholder="email"
-							value={formData.email}
-							onChange={handleChange}
-						/>
-						<TextInput
-							id="jobTitle"
-							name="jobTitle"
-							label="Job Title"
-							placeholder="job title"
-							value={formData.jobTitle}
-							onChange={handleChange}
-						/>
-						<TextInput
-							id="company"
-							name="company"
-							label="Company"
-							placeholder="company"
-							value={formData.company}
-							onChange={handleChange}
-						/>
-					</ul>
+        <form className={styles['form']} onSubmit={handleSubmit}>
+          <ul className={styles['text-input-list']}>
+            <TextInput
+              id="name"
+              name="name"
+              label="Name*"
+              placeholder="name..."
+              value={formData.name}
+              onChange={handleChange}
+            />
+            <TextInput
+              id="email"
+              name="email"
+              label="Email*"
+              placeholder="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <TextInput
+              id="jobTitle"
+              name="jobTitle"
+              label="Job Title"
+              placeholder="job title"
+              value={formData.jobTitle}
+              onChange={handleChange}
+            />
+            <TextInput
+              id="company"
+              name="company"
+              label="Company"
+              placeholder="company"
+              value={formData.company}
+              onChange={handleChange}
+            />
+          </ul>
 
-					<h3>How would you like to partner up?</h3>
+          <h3>How would you like to partner up?</h3>
 
-					<ul className={styles["sponsor-selection-list"]}>
-						<SponsorSelection
-							id="venue"
-							name="venue"
-							label="Venue"
-							icon="venue"
-							checked={formData.venue}
-							onChange={handleChange}
-						/>
-						<SponsorSelection
-							id="food"
-							name="food"
-							label="Food"
-							icon="food"
-							checked={formData.food}
-							onChange={handleChange}
-						/>
-						<SponsorSelection
-							id="drinks"
-							name="drinks"
-							label="Drinks"
-							icon="drinks"
-							checked={formData.drinks}
-							onChange={handleChange}
-						/>
-						<SponsorSelection
-							id="speakers"
-							name="speakers"
-							label="Speakers"
-							icon="speakers"
-							checked={formData.speakers}
-							onChange={handleChange}
-						/>
-						<SponsorSelection
-							id="workshops"
-							name="workshops"
-							label="Workshops"
-							icon="workshops"
-							checked={formData.workshops}
-							onChange={handleChange}
-						/>
-						<SponsorSelection
-							id="other"
-							name="other"
-							label="Other"
-							icon="other"
-							checked={formData.other}
-							onChange={handleChange}
-						/>
-					</ul>
+          <ul className={styles['sponsor-selection-list']}>
+            <SponsorSelection
+              id="venue"
+              name="venue"
+              label="Venue"
+              icon="venue"
+              checked={formData.venue}
+              onChange={handleChange}
+            />
+            <SponsorSelection
+              id="food"
+              name="food"
+              label="Food"
+              icon="food"
+              checked={formData.food}
+              onChange={handleChange}
+            />
+            <SponsorSelection
+              id="drinks"
+              name="drinks"
+              label="Drinks"
+              icon="drinks"
+              checked={formData.drinks}
+              onChange={handleChange}
+            />
+            <SponsorSelection
+              id="speakers"
+              name="speakers"
+              label="Speakers"
+              icon="speakers"
+              checked={formData.speakers}
+              onChange={handleChange}
+            />
+            <SponsorSelection
+              id="workshops"
+              name="workshops"
+              label="Workshops"
+              icon="workshops"
+              checked={formData.workshops}
+              onChange={handleChange}
+            />
+            <SponsorSelection
+              id="other"
+              name="other"
+              label="Other"
+              icon="other"
+              checked={formData.other}
+              onChange={handleChange}
+            />
+          </ul>
 
-					<Button formStatus={pending}>Submit</Button>
-					{/* icon='circle-arrow-right' */}
-				</form>
-			</div>
-		</section>
-	);
+          <SubmitButton label="Submit" disabled={pending} />
+          {/* icon='circle-arrow-right' */}
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Partnerships;
--- src/app/(landingPage)/Speakers/index.tsx
+++ src/app/(landingPage)/Speakers/index.tsx
@@ -1,124 +1,127 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { useFormStatus } from "react-dom";
+import { useState } from 'react';
+import { useFormStatus } from 'react-dom';
 
-import Icon from "@Icons";
-import TextInput from "@Components/TextInput";
-import Button from "@Components/SubmitButton";
+import Icon from '@Icons';
+import TextInput from '@Components/TextInput';
+import SubmitButton from '@Components/SubmitButton';
 
-import styles from "./styles.module.css";
+import styles from './styles.module.css';
 
 interface SpeakerFormTypes {
-	name: string;
-	jobTitle: string;
-	email: string;
-	company: string;
-	topic: string;
-	summary: string;
+  name: string;
+  jobTitle: string;
+  email: string;
+  company: string;
+  topic: string;
+  summary: string;
+  showcase: boolean;
+  instructional: boolean;
+  interactive: boolean;
+  developerPanel: boolean;
+  lightningTalk: boolean;
 }
 
 function Speakers() {
-	const { pending } = useFormStatus();
-	const [formData, setFormData] = useState<SpeakerFormTypes>({
-		name: "",
-		jobTitle: "",
-		email: "",
-		company: "",
-		topic: "",
-		showcase: false,
-		instructional: false,
-		interactive: false,
-		developerPanel: false,
-		lightningTalk: false,
-	});
+  const { pending } = useFormStatus();
+  const [formData, setFormData] = useState<SpeakerFormTypes>({
+    name: '',
+    jobTitle: '',
+    email: '',
+    company: '',
+    topic: '',
+    summary: '',
+    showcase: false,
+    instructional: false,
+    interactive: false,
+    developerPanel: false,
+    lightningTalk: false
+  });
 
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value, type, checked } = e.target;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.target;
 
-		setFormData((prevFormData) => ({
-			...prevFormData,
-			[name]: type === "checkbox" ? checked : value,
-		}));
-	};
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: type === 'checkbox' ? checked : value
+    }));
+  };
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		console.log("formdata", formData);
-	};
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('formdata', formData);
+  };
 
-	return (
-		<section id="speakers" className={styles["layout"]}>
-			<div>
-				<h2>Speakers</h2>
-				<h3>
-					Interested in speaking at our next event? Please fill out the form
-					below
-				</h3>
+  return (
+    <section id="speakers" className={styles['layout']}>
+      <div>
+        <h2>Speakers</h2>
+        <h3>Interested in speaking at our next event? Please fill out the form below</h3>
 
-				<form className={styles["form"]} onSubmit={handleSubmit}>
-					<ul className={styles["text-input-list"]}>
-						<TextInput
-							id="name"
-							name="name"
-							label="Name *"
-							placeholder="name"
-							value={formData.name}
-							onChange={handleChange}
-						/>
-						<TextInput
-							id="email"
-							name="email"
-							label="Email *"
-							placeholder="email"
-							value={formData.email}
-							onChange={handleChange}
-						/>
-						<TextInput
-							id="jobTitle"
-							name="jobTitle"
-							label="Job Title"
-							placeholder="job title"
-							value={formData.jobTitle}
-							onChange={handleChange}
-						/>
-						<TextInput
-							id="company"
-							name="company"
-							label="Company"
-							placeholder="company"
-							value={formData.company}
-							onChange={handleChange}
-						/>
+        <form className={styles['form']} onSubmit={handleSubmit}>
+          <ul className={styles['text-input-list']}>
+            <TextInput
+              id="name"
+              name="name"
+              label="Name *"
+              placeholder="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+            <TextInput
+              id="email"
+              name="email"
+              label="Email *"
+              placeholder="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <TextInput
+              id="jobTitle"
+              name="jobTitle"
+              label="Job Title"
+              placeholder="job title"
+              value={formData.jobTitle}
+              onChange={handleChange}
+            />
+            <TextInput
+              id="company"
+              name="company"
+              label="Company"
+              placeholder="company"
+              value={formData.company}
+              onChange={handleChange}
+            />
 
-						<div className={styles["topic-summary"]}>
-							<TextInput
-								id="topic"
-								name="topic"
-								label="Topic"
-								placeholder="topic"
-								value={formData.topic}
-								onChange={handleChange}
-							/>
+            <div className={styles['topic-summary']}>
+              <TextInput
+                id="topic"
+                name="topic"
+                label="Topic"
+                placeholder="topic"
+                value={formData.topic}
+                onChange={handleChange}
+              />
 
-							<li className={styles["summary"]}>
-								<label htmlFor="summary">Summary</label>
-								<textarea
-									id="summary"
-									name="Summary"
-									value={formData.summary}
-									placeholder="Summary of your talk..."
-									rows={10}
-								/>
-							</li>
-						</div>
-					</ul>
+              <li className={styles['summary']}>
+                <label htmlFor="summary">Summary</label>
+                <textarea
+                  id="summary"
+                  name="Summary"
+                  value={formData.summary}
+                  placeholder="Summary of your talk..."
+                  rows={10}
+                />
+              </li>
+            </div>
+          </ul>
 
-					<Button formStatus={pending}>Submit</Button>
-				</form>
-			</div>
-		</section>
-	);
+          <SubmitButton label="Submit" disabled={pending} />
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Speakers;
